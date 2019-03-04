6542ad7d5051e6b74e170b434821c90d1d45dab2
benAkehurst <benakehurst@hotmail.com>
2019-03-04 14:54:58 +0000

    Added js functions

--- test-ionic-app/app.js
+++ test-ionic-app/app.js
@@ -1,18 +1,41 @@
+// Defines HTML elements for access
 const reasonInput = document.querySelector('#input-reason');
 const amountInput = document.querySelector('#input-amount');
 const cancelBtn = document.querySelector('#button-cancel');
 const confirmBtn = document.querySelector('#button-confirm');
 const expensesList = document.querySelector('#expenses-list');
+const totalExpensesSpan = document.querySelector('#total-expenses');
+
+// Init running total of expenses
+let totalExpenses = 0;
+
+// Clears the input fields
+const clear = () => {
+  reasonInput.value = '';
+  amountInput.value = '';
+}
 
 confirmBtn.addEventListener('click', () => {
+  // Gets input values
   const enteredReason = reasonInput.value;
   const enteredAmount = amountInput.value;
 
-  if(enteredReason.trim().length < 0 || enteredAmount <= 0 || enteredAmount.trim() <= 0) {
+  // Validates inputs aren't empty
+  if(enteredReason.trim().length <= 0 || enteredAmount <= 0 || enteredAmount.trim() <= 0) {
     return;
   }
+  // Adds a new item to list of expenses
   const newItem = document.createElement('ion-item');
   newItem.textContent = enteredReason + ': £' + enteredAmount;
-
   expensesList.appendChild(newItem);
+
+  // Adds up a running total of expenses
+  totalExpenses += +enteredAmount;
+  totalExpensesSpan.textContent = '£' + totalExpenses;
+
+  // Clears the form at the end of the script
+  clear();
 });
+
+// Adds the clear function to the clear button
+cancelBtn.addEventListener('click', clear);
