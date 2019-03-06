3dc332292a4652ba8094622ee18a9119d47637be
benAkehurst <benakehurst@hotmail.com>
2019-03-06 13:54:56 +0000

    Opening and closing a modal

--- final-project/src/app/places/discover/place-detail/place-detail.page.ts
+++ final-project/src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,24 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, ModalController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
 
 import { PlacesService } from '../../places.service';
 import { Place } from '../../place.model';
+import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
 
 @Component({
   selector: 'app-place-detail',
   templateUrl: './place-detail.page.html',
-  styleUrls: ['./place-detail.page.scss'],
+  styleUrls: ['./place-detail.page.scss']
 })
 export class PlaceDetailPage implements OnInit {
-
   public place: Place;
 
   constructor(
     private navCtrl: NavController,
     private activatedRoute: ActivatedRoute,
-    private placesService: PlacesService
-   ) { }
+    private placesService: PlacesService,
+    private modalController: ModalController
+  ) {}
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(paramMap => {
@@ -31,7 +32,19 @@ export class PlaceDetailPage implements OnInit {
   }
 
   public onBookPlace() {
-    this.navCtrl.navigateBack('/places/tabs/discover');
+    this.modalController
+      .create({
+        component: CreateBookingComponent,
+        componentProps: { selectedPlace: this.place }
+      })
+      .then(modalEl => {
+        modalEl.present();
+        return modalEl.onDidDismiss();
+      })
+      .then(resultData => {
+        if (resultData.role === 'confirm') {
+          console.log('Booked');
+        }
+      });
   }
-
 }
